69c23589fb8c3cf305010f4700c42b5649c3221f
Mike Du Russel <ethryx@me.com>
2014-09-15 11:52:14 -0400

    Work on Timeclock page with new heatmap.

--- api/controllers/TimeclockController.js
+++ api/controllers/TimeclockController.js
@@ -4,6 +4,14 @@ module.exports = {
 		res.view({
 			selectedPage: 'timeclock'
 		});
-	}
+	},
+
+  getTimeclockData: function(req, res) {
+    res.send(JSON.stringify({"1410794885": 6}));
+  },
+
+  getDailyTimeclockData: function(req, res) {
+    res.send('hey');
+  }
 
 };
--- assets/js/timeclock.js
+++ assets/js/timeclock.js
@@ -4,22 +4,47 @@ _timeclock.prototype.init = function() {
 	$(document).ready(function(){
 		$('#selOffice').chosen();
 
-		this.demo();
-	}.bind(this));
-};
+		// Timeclock Heatmap
+		var now = new Date();
+		this.tcHeatMap = new CalHeatMap();
+		this.tcHeatMap.init({
+			itemSelector: '#timeclockHeatmap',
+			domain: 'month',
+			subDomain: 'day',
+			start: new Date(now.setMonth(1, 1)),
+			range: 11,
+			rowLimit: 7,
+			cellSize: 13,
+			tooltip: true,
+			highlight: "now",
+			legend: [1, 3, 5, 7, 9, 11, 13],
+			legendColors: { min: "#eee", max: "#59C8F7", empty: "#eee", base: "#eee" },
+			itemName: ["hour", "hours"],
+			data: "/timeclock/getTimeclockData",
+			dataType: "txt",
+			afterLoadData: function(data) {
+				console.log(data);
 
-_timeclock.prototype.demo = function() {
-	$('.barshift').each(function(i, elem) {
-		var barWidth = $(elem).width();
-		var barStart = parseInt($(elem).data('start'));
-		var barEnd = parseInt($(elem).data('end'));
+				return JSON.parse(data);
+			},
+			onClick: function(date, value) {
+				if(!value)
+					return;
 
-		$(elem).css({
-			backgroundColor: $(elem).data('color'),
-			marginLeft: barStart,
-			width: (barEnd - barStart) + 'px',
-			height: '34px',
-			position: 'absolute'
+				new jBox('Modal', {
+					width: 400,
+					height: 300,
+					attach: $('#timeclockDay'),
+					title: '<strong>Timeclock Data:</strong> ' + date.toLocaleString(),
+					animation: {open: 'flip', close: 'flip'},
+					ajax: {
+						url: "/timeclock/getDailyTimeclockData",
+						data: 'ts=' + date.getTime(),
+						spinner: true,
+						setContent: true
+					}
+				}).open();
+			}
 		});
-	});
+	}.bind(this));
 };
