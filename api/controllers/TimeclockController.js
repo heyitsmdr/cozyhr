5e1aee702d24cfc7b858d22895b815304a910f70
Mike Du Russel <ethryx@me.com>
2014-10-24 18:14:48 -0400

    Use new exceptions on timeclock page.

--- api/controllers/TimeclockController.js
+++ api/controllers/TimeclockController.js
@@ -1,54 +1,57 @@
 module.exports = {
 
+  /**
+   * @via     HTTP
+   * @method  GET
+   */
 	index: function(req, res) {
-    Office.find({ company: req.session.userinfo.company.id }, function(e, offices) {
+    var es = ExceptionService.require(req, res, { GET: true });
+
+    Office.find({ company: req.session.userinfo.company.id }).exec(es.wrap(function(e, offices) {
       res.view({
         selectedPage: 'timeclock',
         offices: offices,
         mustacheTemplates: ['clockPositionsFillin', 'noClockedTime']
       });
-    })
+    }));
 	},
 
-  getTimeclockData: function(req, res) {
-    res.send(JSON.stringify({"1410794885": 6}));
-  },
-
-  getDailyTimeclockData: function(req, res) {
-    res.send('hey');
-  },
-
+  /**
+   * @via     Socket
+   * @method  GET
+   */
   getClockablePositions: function(req, res) {
-    if(req.method == 'GET') {
-      var officeId = req.param('officeId');
+    var es = ExceptionService.require(req, res, { socket: true, GET: true });
 
-      if(!officeId) {
-        return res.serverError(new Error('InvalidParameterException'));
+    var officeId = req.param('officeId');
+
+    if(!officeId) {
+      throw ExceptionService.error('No office id specified.');
+    }
+
+    Office.findOne(officeId).exec(es.wrap(function(e, office){
+      if(e || !office) {
+        throw ExceptionService.error('Could not find office.');
       }
 
-      Office.findOne(officeId).exec(function(e, office){
-        if(e || !office) {
-          return res.serverError(new Error('ParameterNotFoundInDatabaseException'));
-        }
+      // same company?
+      if(office.company != req.session.userinfo.company.id) {
+        throw ExceptionService.error('Office does not belong to this company.');
+      }
 
-        // same company?
-        if(office.company != req.session.userinfo.company.id) {
-          return res.serverError(new Error('ParameterCompanyMismatchException'));
-        }
-
-        // Get all the positions for the company's location
-        Position.find({ office: officeId }, function(e, positions) {
-          res.json({"positions": positions});
-        });
-      });
-    } else {
-      return res.json({'error':'Invalid request type. Expected GET.'});
-    }
+      // Get all the positions for the company's location
+      Position.find({ office: officeId }).exec(es.wrap(function(e, positions) {
+        res.json({"positions": positions});
+      }));
+    }));
   },
 
-  /* Request Type: Socket.GET */
+  /**
+   * @via     Socket
+   * @method  GET
+   */
   getClocks: function(req, res) {
-    ExceptionService.require(req, { socket: true, GET: true });
+    ExceptionService.require(req, res, { socket: true, GET: true });
 
     req.socket.emit('clockUpdate', { clocks: [] });
   }
