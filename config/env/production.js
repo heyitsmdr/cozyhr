4cde5012a2df611b7c8eeb2bbdb9bfdefcdf4665
Mike Du Russel <ethryx@me.com>
2014-11-21 12:29:39 -0500

    Production fix for mongo change.

--- config/env/production.js
+++ config/env/production.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-    connection: 'herokuMongoServer'
+    connection: 'digitalOceanMongoServer'
   },
 
   consoleFiltering: {
