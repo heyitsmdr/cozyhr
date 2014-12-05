dd4ec0b956c86c16dd37d5eea2dd942d88e46832
Mike Du Russel <ethryx@me.com>
2014-12-05 09:22:02 -0500

    travis changes

---
+++ test/travis.js
@@ -0,0 +1,17 @@
+var childproc = require('child_process');
+
+var sails = childproc.spawn('node', ['app.js']);
+
+sails.on('close', function() {
+  console.log('Failed');
+  process.exit(1);
+});
+
+sails.stdout.on('data', function(data) {
+  console.log('> ' + data.toString('utf8'));
+
+  if(data.toString('utf8').indexOf('Server lifted') > 0) {
+    console.log('Success');
+    process.exit(0);
+  }
+});
